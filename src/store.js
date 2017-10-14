e74360bacd7730a009176bd7f552d351cb6b1b27
bondiano <batbondik0@gmail.com>
2017-10-14 20:58:26 +0700

    sdas

--- src/store.js
+++ src/store.js
@@ -6,13 +6,8 @@ Vue.use(Vuex);
 
 const userModule = {
   state: {
-<<<<<<< HEAD
     username: 'Meow',
     coins: 500,
-=======
-    username: 'Unknown',
-    coins: 0,
->>>>>>> 06bbb19f06c0aa7e307f7c0268e23e0c0630cc4b
     avatar: 'https://avatars2.githubusercontent.com/u/22221382?s=460&v=4',
   },
 
