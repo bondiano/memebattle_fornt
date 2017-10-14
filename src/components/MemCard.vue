b821de7097dc49d11f081a851b9ac0b74d8fc6e8
StepanDyubin <svd18101997@gmail.com>
2017-10-14 22:07:04 +0700

    arewr we

---
+++ src/components/MemCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="choose">Choose</button>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'UserChooseModal',
+    data() {
+      return {
+//        image: ''
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+      choose() {
+        this.$store.dispatch('socket_chooseMem', {});
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
