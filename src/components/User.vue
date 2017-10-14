5595256f2e89870b71940061a87ca38821bb7a36
StepanDyubin <svd18101997@gmail.com>
2017-10-14 16:56:13 +0700

    arewr we

---
+++ src/components/User.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user">
+    <h4>{{ newUsername }}</h4>
+    <input v-model=newUsername type="text">
+    <button @click="setUsername">Submit</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'User',
+    data() {
+      return {
+        newUsername: 'Unknown'
+      }
+    },
+    computed: {
+      username() {
+        return this.$store.getters.username;
+      }
+    },
+    methods: {
+      setUsername() {
+        this.$store.dispatch('setUsername', this.data.newUsername)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
