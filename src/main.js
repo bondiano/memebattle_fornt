33341f91e5da3980d109ad72f9d900e49026d2b6
StepanDyubin <svd18101997@gmail.com>
2017-10-14 19:11:25 +0700

    arewr we

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueSocketio from 'vue-socket.io';
 import App from './App';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
-
+Vue.use(VueSocketio, 'http://192.168.0.142:8000', store);
 
 new Vue({
   el: '#app',
