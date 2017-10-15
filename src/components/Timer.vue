a416c80ecdd332f7821dadcfeeee047070bf6ea4
bondiano <batbondik0@gmail.com>
2017-10-15 14:07:44 +0700

    sdasasdsafsdf

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,26 +1,24 @@
 <template>
-    <div class="timer" v-if='checked'>
+    <div v-if='checked'>
         <b-container class="bv-example-row">
             <b-row class="text-center">
                 <b-col>
     <h1>До следующего раунда: </h1>
                 </b-col>
                 <b-col>
-    <ol class="clock">
-    <li class="sec">
-        <div class="one">
-            <span>5</span>
-            <span>4</span>
-            <span>3</span>
-            <span>2</span>
-            <span>1</span>
+        <div class="timer">
+        <ul id=timer-ones>
+            <li>5</li>
+            <li>4</li>
+            <li>3</li>
+            <li>2</li>
+            <li>1</li>
+        </ul>
         </div>
-      </li>
-    </ol> 
        </b-col>
-    <b-col>
+    <!-- <b-col>
     <img src="../assets/Pacman.svg"></svg>
-    </b-col>
+    </b-col> -->
     </b-row>
         </b-container>
     </div>
@@ -43,45 +41,31 @@
 </script>
 
 <style>
-.timer ol {
-  list-style: none;
-  text-align: center;
-  margin: 0 auto;
-  padding: 0 0.5em;
-  height: 2.5rem;
-  overflow: hidden;
+.timer { display: inline-block; height: 2.5rem; overflow: hidden; }
+.timer li{
+    color:#FFF;
+    font-family: 'Roboto', Helvetica;
+    font-size:2.5rem; 
 }
+.timer ul {
+    display: inline-block; 
+    list-style-type: none; 
+    width: 2.5rem; 
+    padding-left: 0; 
+    margin-top: 0;
+    position: relative;
+    top: -2.5rem; 
+    line-height: 2.5rem; 
+}
+@keyframes rollover {
+     0% { top: 0px; }
+    20% { top: -2.5rem; }
+    40% { top: -5rem; }
+    60% { top: -7.5rem; }
+    80% { top: -10rem; }
+    100% { top: -12.5rem; }
+}
+#timer-ones {animation: rollover 5s cubic-bezier(1.000,0.005,0.995,0.090) infinite;}
 
-.timer li {
-  float: left;
-  padding: 0 0.5em;
-  background-color: #333;
-  margin: 0 0.5em;
-  font-family: Poiret One, monospace;
-  color: #ddd;
-}
 
-div.timer {
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-}
-
-.timer span {
-  display: block;
-  font-size: 2.5rem;
-  line-height: 1em;
-}
-.sec .one {
-  animation-name: one-step-sec;
-  animation-duration: 5s;
-}
-
-@keyframes one-step-sec {
-  from {
-    top: 0rem;
-  }
-  to {
-    top: -7.5rem; 
-  }
-}
 </style>
